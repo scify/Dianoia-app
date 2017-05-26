97ae04abd19214917c657b3c2d4821c5fa50a305
Pavlos Isaris <paulisaris@gmail.com>
2017-05-26 16:38:06 +0300

    Debug message

--- src/providers/page/page.ts
+++ src/providers/page/page.ts
@@ -12,7 +12,6 @@ export class PageProvider {
   }
 
   getDataForPage(pageCode, pageFile) {
-    console.log("requested file:", this.endPoint + pageFile);
     return this.apiCalls.getHttpCall(pageCode, () => {
       return this.http.get(this.endPoint + pageFile)
         .map(res => res.json());
