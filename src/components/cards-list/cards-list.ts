2f9df7d95b9dcc4d355739a339b9b23333562dd8
Paul Isaris <paulisaris@gmail.com>
2021-11-03 16:50:40 +0200

    Added translation for activities text

--- src/components/cards-list/cards-list.ts
+++ src/components/cards-list/cards-list.ts
@@ -1,4 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Events, Platform} from "ionic-angular";
+import {TranslateService} from "@ngx-translate/core";
 
 /**
  * Generated class for the CardsListComponent component.
@@ -11,18 +13,38 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   templateUrl: 'cards-list.html'
 })
 export class CardsListComponent {
-
+  activityTranslationSingular: string = '';
+  activityTranslationPlural: string = '';
   @Input('cards') cards: [any];
   @Output() cardClick = new EventEmitter();
 
-  constructor() {
+  constructor(public platform: Platform, private translate: TranslateService,
+              public events: Events) {
+
+  }
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.translate.get('activity_translation_singular').subscribe((translated) => {
+        this.activityTranslationSingular = translated;
+        this.activityTranslationPlural = this.translate.instant('activity_translation_plural');
+      });
+    });
+    this.events.subscribe('lang_ready', (langCode) => {
+      this.activityTranslationSingular = this.translate.instant('activity_translation_singular');
+      this.activityTranslationPlural = this.translate.instant('activity_translation_plural');
+    });
+  }
+
+  ngOnDestroy() {
+
   }
 
   getCardsListTitle() {
     let title = this.cards.length;
     if(!this.cards) return '';
     if(this.cards[0].id) {
-      return this.cards.length == 1 ? title + " δραστηριότητα:" : title + " δραστηριότητες:";
+      return this.cards.length == 1 ? title + " " + this.activityTranslationSingular + ":" : title + " " + this.activityTranslationPlural + ":";
     }
   }
 
